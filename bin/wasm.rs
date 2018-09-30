48666072e57c5a73534d68ca43024d137f4db71e
petr-tik <petr-tik@users.noreply.github.com>
2018-09-30 22:35:25 +0100

    Add the new wasm target

---
+++ bin/wasm.rs
@@ -0,0 +1,60 @@
+extern crate wasm_bindgen;
+
+extern crate tantivy;
+
+use tantivy::collector::TopCollector;
+use tantivy::directory::static_directory::StaticDirectory;
+use tantivy::query::QueryParser;
+use tantivy::Index;
+use tantivy::Error;
+
+use wasm_bindgen::prelude::*;
+
+fn instantiate_index(data: &'static [u8]) -> Result<Index, tantivy::Error> {
+    let directory = StaticDirectory::open(data)?;
+    let index = Index::open(directory)?;
+    index.load_searchers()?;
+    Ok(index)
+}
+
+
+#[wasm_bindgen]
+pub fn query(query: &str) -> Vec<String> {
+    // Prints each argument on a separate line
+    let data: &'static [u8] = &[10, 12, 15];
+    let index = instantiate_index(data).unwrap();
+    let searcher = index.searcher();
+
+    let schema = index.schema();
+    let command = schema.get_field("command").unwrap();
+    let text = schema.get_field("text").unwrap();
+
+    let query_parser = QueryParser::for_index(&index, vec![command, text]);
+    let query = query_parser.parse_query(query).unwrap();
+
+    let mut top_collector = TopCollector::with_limit(10);
+
+    searcher.search(&*query, &mut top_collector).unwrap();
+
+    let doc_addresses = top_collector.docs();
+
+    // The actual documents still need to be
+
+    // retrieved from Tantivy's store.
+
+    //
+    // Since the body field was not configured as stored,
+    // the document returned will only contain
+    // a title.
+    let mut docs = Vec::new();
+
+    for doc_address in doc_addresses {
+        let retrieved_doc = searcher.doc(doc_address).unwrap();
+        docs.push(schema.to_json(&retrieved_doc));
+    }
+
+    docs
+}
+
+#[cfg(test)]
+mod tests {}
